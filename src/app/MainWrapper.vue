864adb17a0efa37c3077e6271faa373baf9db496
kirill.kotov <txrB7rjWPM>
2022-03-21 10:11:52 +0300

    create app

---
+++ src/app/MainWrapper.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-wrapper container">
+    <h1 class="main-wrapper__title">Simon Game</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainWrapper"
+}
+</script>
+
+<style lang="scss" scoped>
+.main-wrapper {
+  padding: 20px;
+
+  &__title {
+    font-family: Raleway, sans-serif;
+    font-size: 48px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 48px;
+    letter-spacing: 0em;
+    text-align: center;
+
+    color: #000000;
+
+    margin-bottom: 24px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from "@/router/router";
+
 Vue.config.productionTip = false
+Vue.config.devtools  = true
+
+
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
