701db105316c680c1e4bc16c2258272c7563394f
kirill.kotov <txrB7rjWPM>
2022-03-25 10:54:58 +0300

    deploy

---
+++ js/125.845cba82.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksimon_game"]=self["webpackChunksimon_game"]||[]).push([[125],{5125:function(e,a,n){n.r(a),n.d(a,{default:function(){return c}});var t=function(){var e=this,a=e.$createElement,n=e._self._c||a;return n("div",{staticClass:"main-wrapper container"},[n("h1",{staticClass:"main-wrapper__title"},[e._v("Simon Game")]),n("router-view")],1)},r=[],i={name:"MainWrapper"},s=i,u=n(1001),l=(0,u.Z)(s,t,r,!1,null,"da81f77e",null),c=l.exports}}]);
+//# sourceMappingURL=125.845cba82.js.map
